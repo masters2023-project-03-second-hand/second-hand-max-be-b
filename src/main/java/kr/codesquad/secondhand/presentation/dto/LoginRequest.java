f20a76d52d54d9ae9433ecbcc6b9393668b2fb68
HeeJu Cho <121915790+he2joojo@users.noreply.github.com>
2023-08-23 22:07:42 +0900

    #7 feat: 회원가입 기능 개발
    - loginId가 중복되는지 검증 후 회원을 저장합니다.

--- src/main/java/kr/codesquad/secondhand/presentation/dto/LoginRequest.java
+++ src/main/java/kr/codesquad/secondhand/presentation/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package kr.codesquad.secondhand.presentation.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginRequest {
 
+    @NotBlank
+    @Size(min = 2, max = 12) // todo: ExceptionHandler에서 예외처리하기
     private String loginId;
 
     public LoginRequest(String loginId) {
