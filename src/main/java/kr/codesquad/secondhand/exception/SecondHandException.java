1ac27936a91d0744e8f9df9862bb9059b816edb3
Jeongyong Park <1239824@naver.com>
2023-08-24 10:45:01 +0900

    feat: 예외 클래스 생성

--- src/main/java/kr/codesquad/secondhand/exception/SecondHandException.java
+++ src/main/java/kr/codesquad/secondhand/exception/SecondHandException.java
@@ -6,8 +6,15 @@ import lombok.Getter;
 public class SecondHandException extends RuntimeException {
 
     private final ErrorCode errorCode;
+    private final String message;
 
     public SecondHandException(ErrorCode errorCode) {
         this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+    }
+
+    public SecondHandException(ErrorCode errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
     }
 }
