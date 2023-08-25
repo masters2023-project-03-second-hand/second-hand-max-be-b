b98631f090ee40c458ee2b817581104757b7f2af
JeongYong Park <66981851+23Yong@users.noreply.github.com>
2023-08-25 09:38:23 +0900

    이미지 업로드 (#11)
    * chore: aws s3 업로드를 위한 dependency 추가
    
    * feat: aws 환경변수 설정 및 프로퍼티 클래스 설정
    
    * feat: 예외 클래스 생성
    
    * feat: 예외 처리 클래스 생성
    
    * feat: S3 업로드를 위한 설정 클래스 생성
    
    * feat: 이미지 업로드 기능구현
    
    * test: 이미지 업로드 테스트코드 작성
    
    * refactor: 생성자 접근제어자 수정

---
+++ src/test/java/kr/codesquad/secondhand/domain/image/ImageFileTest.java
@@ -0,0 +1,48 @@
+package kr.codesquad.secondhand.domain.image;
+
+import kr.codesquad.secondhand.exception.BadRequestException;
+import kr.codesquad.secondhand.exception.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ImageFileTest {
+
+    @DisplayName("올바른 이미지 파일이 들어와 ImageFile 인스턴스 생성에 성공한다.")
+    @Test
+    void givenMockImageFile_whenCreateImageFile_thenSuccess() {
+        // given
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "test-image",
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "imageBytes".getBytes(StandardCharsets.UTF_8)
+        );
+
+        // when & then
+        assertThatCode(() -> ImageFile.from(mockMultipartFile)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("지원하지 않는 이미지 확장자가 들어오면 예외를 던진다.")
+    @Test
+    void givenNotSupportedFileExtension_whenCreateImageFile_thenThrowsException() {
+        // given
+        MockMultipartFile mockMultipartFile = new MockMultipartFile(
+                "test-image",
+                "image.gif",
+                MediaType.IMAGE_GIF_VALUE,
+                "imageBytes".getBytes(StandardCharsets.UTF_8)
+        );
+
+        // when & then
+        assertThatThrownBy(() -> ImageFile.from(mockMultipartFile))
+                .isInstanceOf(BadRequestException.class)
+                .extracting("errorCode").isEqualTo(ErrorCode.INVALID_FILE_EXTENSION);
+    }
+}
