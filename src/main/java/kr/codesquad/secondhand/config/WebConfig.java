526b01a9bacb22707fc87b005b2eeb249569d3b4
Jeongyong Park <1239824@naver.com>
2023-08-28 14:01:26 +0900

    feat: argument resolver 등록

---
+++ src/main/java/kr/codesquad/secondhand/config/WebConfig.java
@@ -0,0 +1,20 @@
+package kr.codesquad.secondhand.config;
+
+import java.util.List;
+import kr.codesquad.secondhand.presentation.support.AuthArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthArgumentResolver authArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
+    }
+}
