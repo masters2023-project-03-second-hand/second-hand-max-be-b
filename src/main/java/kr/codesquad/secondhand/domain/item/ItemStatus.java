0f090d8427436c8f60ad67577a14d9d6ffe49388
Jeongyong Park <1239824@naver.com>
2023-08-28 17:46:54 +0900

    feat: 상품 엔티티 클래스 작성

---
+++ src/main/java/kr/codesquad/secondhand/domain/item/ItemStatus.java
@@ -0,0 +1,25 @@
+package kr.codesquad.secondhand.domain.item;
+
+import java.util.Arrays;
+import kr.codesquad.secondhand.exception.BadRequestException;
+import kr.codesquad.secondhand.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum ItemStatus {
+
+    ON_SALE("판매중"), SOLD_OUT("판매완료"), RESERVED("예약중");
+
+    private final String status;
+
+    public static ItemStatus of(String statusName) {
+        return Arrays.stream(ItemStatus.values())
+                .filter(itemStatus -> itemStatus.getStatus().equals(statusName))
+                .findFirst()
+                .orElseThrow(() -> new BadRequestException(
+                        ErrorCode.INVALID_REQUEST,
+                        "상품 판매 상태는 (판매중, 판매완료, 예약중) 만 들어올 수 있습니다."));
+    }
+}
