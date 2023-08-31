74c2fa927e7d0f8670b87433bdc4d300c97bacd2
Jeongyong Park <1239824@naver.com>
2023-08-31 12:22:42 +0900

    test: 회원가입 인수테스트 작성

--- src/test/java/kr/codesquad/secondhand/acceptance/AuthAcceptanceTest.java
+++ src/test/java/kr/codesquad/secondhand/acceptance/AuthAcceptanceTest.java
@@ -7,7 +7,13 @@ import static org.mockito.BDDMockito.anyString;
 import static org.mockito.BDDMockito.given;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import kr.codesquad.secondhand.domain.image.ImageFile;
 import kr.codesquad.secondhand.domain.member.UserProfile;
 import kr.codesquad.secondhand.presentation.dto.OauthTokenResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +23,17 @@ import org.springframework.http.MediaType;
 
 public class AuthAcceptanceTest extends AcceptanceTestSupport {
 
+    private void mockingOAuth() {
+        given(naverRequester.getToken(anyString()))
+                .willReturn(new OauthTokenResponse("accessToken", "email", "bearer"));
+        given(naverRequester.getUserProfile(any(OauthTokenResponse.class)))
+                .willReturn(new UserProfile("23Yong@secondhand.com", "profileUrl"));
+    }
+
+    private File createFakeFile() throws IOException {
+        return File.createTempFile("test-image", ".png");
+    }
+
     @DisplayName("로그인할 때")
     @Nested
     class Login {
@@ -25,10 +42,7 @@ public class AuthAcceptanceTest extends AcceptanceTestSupport {
         @Test
         void givenLoginData_whenLogin_thenSuccess() {
             // given
-            given(naverRequester.getToken(anyString()))
-                    .willReturn(new OauthTokenResponse("accessToken", "email", "bearer"));
-            given(naverRequester.getUserProfile(any(OauthTokenResponse.class)))
-                    .willReturn(new UserProfile("23Yong@secondhand.com", "profileUrl"));
+            mockingOAuth();
 
             signup();
 
@@ -40,10 +54,7 @@ public class AuthAcceptanceTest extends AcceptanceTestSupport {
                     .body(Map.of("loginId", "23yong"));
 
             // when
-            var response = request
-                    .post("/api/auth/naver/login")
-                    .then().log().all()
-                    .extract();
+            var response = login(request);
 
             // then
             assertAll(
@@ -57,7 +68,7 @@ public class AuthAcceptanceTest extends AcceptanceTestSupport {
 
         @DisplayName("인가코드가 주어지지 않으면 400 응답이 온다.")
         @Test
-        void givenLoginData_whenLogin_thenResponse400() {
+        void givenNoAuthorizationCode_whenLogin_thenResponse400() {
             // given
             var request = RestAssured
                     .given().log().all()
@@ -66,10 +77,7 @@ public class AuthAcceptanceTest extends AcceptanceTestSupport {
                     .body(Map.of("loginId", "23yong"));
 
             // when
-            var response = request
-                    .post("/api/auth/naver/login")
-                    .then().log().all()
-                    .extract();
+            var response = login(request);
 
             // then
             assertThat(response.statusCode()).isEqualTo(400);
@@ -79,10 +87,7 @@ public class AuthAcceptanceTest extends AcceptanceTestSupport {
         @Test
         void givenInvalidLoginData_whenLogin_thenResponse401() {
             // given
-            given(naverRequester.getToken(anyString()))
-                    .willReturn(new OauthTokenResponse("accessToken", "email", "bearer"));
-            given(naverRequester.getUserProfile(any(OauthTokenResponse.class)))
-                    .willReturn(new UserProfile("23Yong@secondhand.com", "profileUrl"));
+            mockingOAuth();
 
             signup();
 
@@ -94,13 +99,76 @@ public class AuthAcceptanceTest extends AcceptanceTestSupport {
                     .body(Map.of("loginId", "joy"));
 
             // when
-            var response = request
+            var response = login(request);
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(401);
+        }
+
+        private ExtractableResponse<Response> login(RequestSpecification request) {
+            return request
                     .post("/api/auth/naver/login")
+                    .then().log().all()
+                    .extract();
+        }
+    }
+
+    @DisplayName("회원가입할 때")
+    @Nested
+    class Signup {
+
+        @DisplayName("올바른 회원가입 정보가 주어지면 회원가입에 성공한다.")
+        @Test
+        void givenSignupData_whenSignup_thenSuccess() throws IOException {
+            // given
+            mockingOAuth();
+            given(s3Uploader.uploadImageFile(any(ImageFile.class))).willReturn("profileUrl");
+
+            var request = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                    .queryParam("code", "code")
+                    .queryParam("state", "state")
+                    .multiPart("request", Map.of("loginId", "bruni", "addrName", "범박동"),
+                            MediaType.APPLICATION_JSON_VALUE)
+                    .multiPart("profile", createFakeFile(),
+                            MediaType.IMAGE_PNG_VALUE);
+
+            // when
+            var response = request
+                    .when()
+                    .post("/api/auth/naver/signup")
                     .then().log().all()
                     .extract();
 
             // then
-            assertThat(response.statusCode()).isEqualTo(401);
+            assertThat(response.statusCode()).isEqualTo(201);
+        }
+
+        @DisplayName("인가코드가 주어지지 않으면 400응답을 한다.")
+        @Test
+        void givenNoAuthorizationCode_whenSignup_thenResponse400() throws IOException {
+            // given
+            var request = RestAssured
+                    .given().log().all()
+                    .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                    .queryParam("state", "state")
+                    .multiPart("request",
+                            Map.of("loginId", "bruni", "addrName", "범박동"),
+                            MediaType.APPLICATION_JSON_VALUE)
+                    .multiPart("profile",
+                            createFakeFile(),
+                            MediaType.IMAGE_PNG_VALUE);
+
+            // when
+            var response = request
+                    .when()
+                    .post("/api/auth/naver/signup")
+                    .then().log().all()
+                    .extract();
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(400);
         }
     }
 }
