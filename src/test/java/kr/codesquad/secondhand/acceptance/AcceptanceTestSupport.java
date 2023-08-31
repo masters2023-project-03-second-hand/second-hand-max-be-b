8082b71dcca8787e2cd01b45d81cfb06571a3278
Jeongyong Park <1239824@naver.com>
2023-08-31 12:02:03 +0900

    refactor: Spring Application Context가 한 번만 띄워지도록 개선

---
+++ src/test/java/kr/codesquad/secondhand/acceptance/AcceptanceTestSupport.java
@@ -0,0 +1,30 @@
+package kr.codesquad.secondhand.acceptance;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.codesquad.secondhand.SupportRepository;
+import kr.codesquad.secondhand.application.auth.NaverRequester;
+import kr.codesquad.secondhand.application.image.S3Uploader;
+import kr.codesquad.secondhand.domain.member.Member;
+import kr.codesquad.secondhand.fixture.FixtureFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@AcceptanceTest
+public abstract class AcceptanceTestSupport {
+
+    @MockBean
+    protected S3Uploader s3Uploader;
+
+    @MockBean
+    protected NaverRequester naverRequester;
+
+    @Autowired
+    protected SupportRepository supportRepository;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected Member signup() {
+        return supportRepository.save(FixtureFactory.createMember());
+    }
+}
