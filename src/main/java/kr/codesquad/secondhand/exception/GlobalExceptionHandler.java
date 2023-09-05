c704c18031aa4afab3a76140b00bd2aaec0bb115
Jeongyong Park <1239824@naver.com>
2023-09-05 12:13:52 +0900

    feat: 잘못된 파라미터에 대한 예외처리

--- src/main/java/kr/codesquad/secondhand/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/codesquad/secondhand/exception/GlobalExceptionHandler.java
@@ -2,11 +2,19 @@ package kr.codesquad.secondhand.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidException(MethodArgumentNotValidException e) {
+        String exMessage = e.getBindingResult().getFieldError().getDefaultMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(400, exMessage));
+    }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
