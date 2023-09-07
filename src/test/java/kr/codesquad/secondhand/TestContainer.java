7035653c26f928e97127241b3a24d18dd40bf660
JeongYong Park <66981851+23Yong@users.noreply.github.com>
2023-09-07 10:45:00 +0900

    테스트 컨테이너 적용 (#64)
    * feat: 테스트 컨테이너 적용
    
    * chore: 배포 전 테스트를 수행하도록 변경
    
    * chore: JDK 설치
    
    * docs: 테스트시 로그를 안찍도록 변경
    
    * chore: redis test container 추가
    
    * chore: testImplementation으로 변경
    
    * docs: yml파일에서 불필요한 정보 제거
    
    * feat: 불필요한 애노테이션 제거

---
+++ src/test/java/kr/codesquad/secondhand/TestContainer.java
@@ -0,0 +1,29 @@
+package kr.codesquad.secondhand;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public abstract class TestContainer {
+
+    static final GenericContainer redis = new GenericContainer(DockerImageName.parse("redis"))
+            .withExposedPorts(6379);
+
+    static final MySQLContainer mySQLContainer = new MySQLContainer(DockerImageName.parse("mysql:8.0.34"))
+            .withDatabaseName("second_hand")
+            .withUsername("root")
+            .withPassword("root");
+
+    static {
+        redis.start();
+        mySQLContainer.start();
+    }
+
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("redis.host", redis::getHost);
+        registry.add("redis.port", () -> redis.getMappedPort(6379));
+    }
+}
--- src/test/java/kr/codesquad/secondhand/application/ApplicationTestSupport.java
+++ src/test/java/kr/codesquad/secondhand/application/ApplicationTestSupport.java
@@ -1,13 +1,14 @@
 package kr.codesquad.secondhand.application;
 
 import kr.codesquad.secondhand.SupportRepository;
+import kr.codesquad.secondhand.TestContainer;
 import kr.codesquad.secondhand.application.auth.NaverRequester;
 import kr.codesquad.secondhand.application.image.S3Uploader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @ApplicationTest
-public abstract class ApplicationTestSupport {
+public abstract class ApplicationTestSupport extends TestContainer {
 
     @MockBean
     protected S3Uploader s3Uploader;
