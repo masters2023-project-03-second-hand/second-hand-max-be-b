b2d70f1e6a73659e073bb86cab82bfd9507f773f
JeongYong Park <66981851+23Yong@users.noreply.github.com>
2023-09-12 10:17:30 +0900

    CORS 설정 (#75)
    * feat: cors 설정 추가
    
    * docs: secret 파일 수정
    
    * feat: 허용 Origin 추가
    
    * feat: 허용 Origin 추가
    
    * refactor: 허용 origin을 환경변수로 추출
    
    * fix: test 돌아가도록 수정

--- src/main/java/kr/codesquad/secondhand/config/WebConfig.java
+++ src/main/java/kr/codesquad/secondhand/config/WebConfig.java
@@ -4,8 +4,10 @@ import java.util.List;
 import kr.codesquad.secondhand.presentation.support.AuthArgumentResolver;
 import kr.codesquad.secondhand.presentation.support.NotNullParamArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
@@ -15,9 +17,21 @@ public class WebConfig implements WebMvcConfigurer {
     private final AuthArgumentResolver authArgumentResolver;
     private final NotNullParamArgumentResolver notNullParamArgumentResolver;
 
+    @Value("${custom.front-local-url}")
+    private String frontLocalUrl;
+    @Value("${custom.front-server-url}")
+    private String frontServerUrl;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authArgumentResolver);
         resolvers.add(notNullParamArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(frontLocalUrl, frontServerUrl)
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH");
     }
 }
