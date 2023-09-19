4e99ff920a9386e2bbdb80a1a3b20307bb0cc566
Jeongyong Park <1239824@naver.com>
2023-09-19 18:28:56 +0900

    test: 인증 관련 테스트코드 작성

--- src/main/java/kr/codesquad/secondhand/presentation/dto/member/SignUpRequest.java
+++ src/main/java/kr/codesquad/secondhand/presentation/dto/member/SignUpRequest.java
@@ -5,11 +5,13 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import kr.codesquad.secondhand.domain.member.Member;
 import kr.codesquad.secondhand.domain.member.UserProfile;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequest {
 
     @NotBlank
