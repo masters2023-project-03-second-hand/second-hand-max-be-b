cd1f87a52e59cf754e18183665ba1c323908a8db
HeeJu Cho <121915790+he2joojo@users.noreply.github.com>
2023-09-22 16:16:14 +0900

    style: @JsonProperty(value = "isSelected") 추가

--- src/main/java/kr/codesquad/secondhand/presentation/dto/member/AddressData.java
+++ src/main/java/kr/codesquad/secondhand/presentation/dto/member/AddressData.java
@@ -1,5 +1,6 @@
 package kr.codesquad.secondhand.presentation.dto.member;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,5 +13,6 @@ public class AddressData {
     private Long addressId;
     private String fullAddressName;
     private String addressName;
+    @JsonProperty(value = "isSelected")
     private boolean isSelected;
 }
