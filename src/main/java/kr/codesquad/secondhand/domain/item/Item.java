a96c30c5dc7c51e621b51b4dc5461001421d5e1f
Jeongyong Park <1239824@naver.com>
2023-09-25 10:26:33 +0900

    style: 네이밍 수정

--- src/main/java/kr/codesquad/secondhand/domain/item/Item.java
+++ src/main/java/kr/codesquad/secondhand/domain/item/Item.java
@@ -100,7 +100,7 @@ public class Item extends AuditingFields {
         this.content = request.getContent();
         this.price = request.getPrice();
         this.tradingRegion = request.getRegion();
-        this.status = ItemStatus.of(request.getStatus());
+        this.status = ItemStatus.fromKorean(request.getStatus());
         this.categoryName = request.getCategoryName();
     }
 
@@ -109,7 +109,7 @@ public class Item extends AuditingFields {
     }
 
     public void changeStatus(String status) {
-        this.status = ItemStatus.of(status);
+        this.status = ItemStatus.fromKorean(status);
     }
 
     public void increaseWishCount() {
