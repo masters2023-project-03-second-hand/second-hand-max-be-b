b45fd2fdb3744244a7a351655eedb6a3513c06c4
Jeongyong Park <1239824@naver.com>
2023-09-27 16:15:31 +0900

    feat: 비관적 락을 사용한 동시성 문제 해결

--- src/main/java/kr/codesquad/secondhand/application/item/event/ItemEventListener.java
+++ src/main/java/kr/codesquad/secondhand/application/item/event/ItemEventListener.java
@@ -1,17 +1,26 @@
 package kr.codesquad.secondhand.application.item.event;
 
+import kr.codesquad.secondhand.domain.item.Item;
+import kr.codesquad.secondhand.exception.ErrorCode;
+import kr.codesquad.secondhand.exception.NotFoundException;
+import kr.codesquad.secondhand.repository.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @RequiredArgsConstructor
 @Component
 public class ItemEventListener {
 
-    private final SynchronizedService synchronizedService;
+    private final ItemRepository itemRepository;
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener
-    public synchronized void increaseViewCount(ItemViewEvent event) {
-        synchronizedService.increaseViewCount(event.getItemId());
+    public void increaseViewCount(ItemViewEvent event) {
+        Item item = itemRepository.findByIdWithPessimisticLock(event.getItemId())
+                .orElseThrow(() -> NotFoundException.itemNotFound(ErrorCode.NOT_FOUND, event.getItemId()));
+        item.incrementViewCount();
     }
 }
--- src/main/java/kr/codesquad/secondhand/repository/item/ItemRepository.java
+++ src/main/java/kr/codesquad/secondhand/repository/item/ItemRepository.java
@@ -1,9 +1,16 @@
 package kr.codesquad.secondhand.repository.item;
 
+import java.util.Optional;
+import javax.persistence.LockModeType;
 import kr.codesquad.secondhand.domain.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT item FROM Item item WHERE item.id = :itemId")
+    Optional<Item> findByIdWithPessimisticLock(@Param("itemId") Long itemId);
 }
